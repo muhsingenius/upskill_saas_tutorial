196a14f16ccd1dcc7f6922a08a0bf528f695d4c4
muhsingenius <muhsingenius@gmail.com>
2017-01-12 00:36:16 +0000

    Added contact form mailer form

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,11 @@
+class ContactMailer < ActionMailer::Base
+  default to: 'muhsingenius@gmail.com'
+ 
+ def contact_mail(name, email, body)
+   @name = name
+   @email = email
+   @body = body
+   
+   mail(from: email, subject: 'Contact Form Message')
+ end
+end
