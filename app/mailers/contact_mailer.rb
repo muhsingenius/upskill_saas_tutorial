e9c1797be902dc9921355528b744fb50f8d3fb5c
muhsingenius <muhsingenius@gmail.com>
2017-01-13 22:34:42 +0000

    added e to contact_mailer.rb

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ActionMailer::Base
   default to: 'muhsingenius@gmail.com'
  
- def contact_mail(name, email, body)
+ def contact_email(name, email, body)
    @name = name
    @email = email
    @body = body
