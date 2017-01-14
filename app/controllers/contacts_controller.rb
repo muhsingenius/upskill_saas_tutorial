ff19717b803808724bedfbbf11c2bd30ed1c5106
muhsingenius <muhsingenius@gmail.com>
2017-01-14 20:40:34 +0000

    added Devise gem, User model and Devise Views

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,25 +1,39 @@
 class ContactsController < ApplicationController 
+  
+  #Get request to /contact-us
+  #Show new contact form
   def new
     @contact = Contact.new  
   end
   
-  def create    
+  #POST response to /contacts
+  def create   
+    #mass asignment of form fields into contact object
     @contact = Contact.new(contact_params)
+    #Save the contact object to the database
     if @contact.save
+      #store form fields via parameters into variables to be used in mailer
       name = params[:contact][:name]
       email = params[:contact][:email]
       body = params[:contact][:comments]
+      #Plug variables into contact mailer email
+      #and sent email
       ContactMailer.contact_email(name, email, body).deliver
       
       flash[:success] = "Message Saved"
       redirect_to new_contact_path
     else
+      #if contact object doesnt save,
+      #store errors in flash hash
+      #and redirect to new action
       flash[:danger] = @contact.errors.full_messages.join(", ")
       redirect_to new_contact_path
     end
   end
   
-  private       
+  private   
+  #strong parameters to collect data from form fields
+  #and whitelist the form for security
     def contact_params
       params.require(:contact).permit(:name, :email, :comments)      
     end
